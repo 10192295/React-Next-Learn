514ee6ca6e3536eafdbf0a4e87b9b9f00716d27b
likai <10192295likai@cn.tre-inc.com>
2023-05-15 13:23:42 +0900

    delete useless code

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,21 +1,19 @@
-import { NextPageWithLayout } from "../page";
 import PrimaryLayout from '@/components/layout/primary/PrimaryLayout';
 import SidebarLayout from '@/components/layout/sidebar/SidebarLayout';
 import { mockSidebarLayoutProps } from '@/components/layout/sidebar/SidebarLayout.mocks';
+import { NextPageWithLayout } from '../page';
 
-const About: NextPageWithLayout = () => {
-  return (
-    <>
-      <div>
-        About
-      </div>
-    </>
-  )
-}
+const Menu: NextPageWithLayout = () => {
+return (
+  <section>
+    <h2>Layout Example (Menu)</h2>
+  </section>
+);
+};
 
-export default About
+export default Menu;
 
-About.getLayout = (page) => {
+Menu.getLayout = (page) => {
   return (
     <PrimaryLayout>
       <SidebarLayout {...mockSidebarLayoutProps.base} />
